fa1980662cdc93fa4c609f3844c39284d6f861b2
Gavin Dold <gdold42@gmail.com>
2015-12-10 17:17:10 +0000

    Implemented --out arg to optionally save to file

--- greengraph.py
+++ greengraph.py
@@ -5,15 +5,19 @@ from argparse import ArgumentParser
 
 if __name__ == "__main__":
     parser = ArgumentParser(description = "Generates a graph of the proportion of green pixels in a series of satellite images between two points")
-    parser.add_argument('--from', '-f', dest='start', default='New York') # Python doesn't like 'arguments.from'
-    parser.add_argument('--to','-t', default='Chicago')
-    parser.add_argument('--steps','-s',default=20)
-    parser.add_argument('--out','-o') # Not yet implemented
+    parser.add_argument('--from', '-f', dest='start', required=True) # Python doesn't like 'arguments.from'
+    parser.add_argument('--to', '-t', required=True)
+    parser.add_argument('--steps', '-s', default=20)
+    parser.add_argument('--out', '-o')
+    parser.add_argument('--format', default=None)
     arguments=parser.parse_args()
     mygraph=Greengraph(arguments.start,arguments.to) 
     data = mygraph.green_between(arguments.steps)
     plt.plot(data)
-    plt.show()
+    if arguments.out:
+        plt.savefig(arguments.out,format=arguments.format)
+    else:
+        plt.show()
 
 # mygraph=Greengraph('New York','Chicago')
 # data = mygraph.green_between(20)
