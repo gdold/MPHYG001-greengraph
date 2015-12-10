b24b19426dcc41938b260e13056405915bc13051
Gavin Dold <gdold42@gmail.com>
2015-12-10 17:32:46 +0000

    Restructured folders in preparation for packaging

---
+++ greengraph/greengraph.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import matplotlib.pyplot as plt
+from graph import Greengraph
+from argparse import ArgumentParser
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description = "Generates a graph of the proportion of green pixels in a series of satellite images between two points")
+    parser.add_argument('--from', dest='start', default='New York', help='Starting location, defaults to New York') # Python doesn't like 'arguments.from'
+    parser.add_argument('--to', default='Chicago', help='End location, defaults to Chicago')
+    parser.add_argument('--steps', default=20, help='Number of steps between start and end locations')
+    parser.add_argument('--out', help='Filename to save plot to. Displays plot on screen instead if not specified')
+    parser.add_argument('--format', default=None, help='Optionally force an output format, overriding extension in --out')
+    arguments=parser.parse_args()
+    mygraph=Greengraph(arguments.start,arguments.to) 
+    data = mygraph.green_between(arguments.steps)
+    plt.plot(data)
+    if arguments.out:
+        plt.savefig(arguments.out,format=arguments.format)
+    else:
+        plt.show()
+
+# mygraph=Greengraph('New York','Chicago')
+# data = mygraph.green_between(20)
+# plt.plot(data)
+# plt.show()
---
+++ greengraph/test/test_greengraph.py
@@ -0,0 +1,5 @@
+from nose.tools import assert_equal, assert_almost_equal
+from ..graph import Greengraph
+from ..map import Map
+from mock import patch
+
