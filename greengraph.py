736ea778388a185098fad25565178063f0ebef85
Gavin Dold <gdold42@gmail.com>
2015-12-10 17:18:43 +0000

    Added help messages to argparse

--- greengraph.py
+++ greengraph.py
@@ -5,11 +5,11 @@ from argparse import ArgumentParser
 
 if __name__ == "__main__":
     parser = ArgumentParser(description = "Generates a graph of the proportion of green pixels in a series of satellite images between two points")
-    parser.add_argument('--from', '-f', dest='start', required=True) # Python doesn't like 'arguments.from'
-    parser.add_argument('--to', '-t', required=True)
-    parser.add_argument('--steps', '-s', default=20)
-    parser.add_argument('--out', '-o')
-    parser.add_argument('--format', default=None)
+    parser.add_argument('--from', '-f', dest='start', required=True, help='Starting location') # Python doesn't like 'arguments.from'
+    parser.add_argument('--to', '-t', required=True, help='End location')
+    parser.add_argument('--steps', '-s', default=20, help='Number of steps between start and end locations')
+    parser.add_argument('--out', '-o', help='Filename to save plot to. Displays plot on screen instead if not specified')
+    parser.add_argument('--format', default=None, help='Optionally force an output format, overriding extension in --out')
     arguments=parser.parse_args()
     mygraph=Greengraph(arguments.start,arguments.to) 
     data = mygraph.green_between(arguments.steps)
