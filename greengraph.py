4d1d67844d90fbb8694ad668e4ddc1f2f6c62858
Gavin Dold <gdold42@gmail.com>
2015-12-10 16:57:26 +0000

    Implemented argparse

--- greengraph.py
+++ greengraph.py
@@ -1,7 +1,21 @@
+#!/usr/bin/env python
 import matplotlib.pyplot as plt
 from graph import Greengraph
+from argparse import ArgumentParser
 
-mygraph=Greengraph('New York','Chicago')
-data = mygraph.green_between(20)
-plt.plot(data)
-plt.show()
+if __name__ == "__main__":
+    parser = ArgumentParser(description = "Generates a graph of the proportion of green pixels in a series of satellite images between two points")
+    parser.add_argument('--from', '-f', dest='start', default='New York') # Python doesn't like 'arguments.from'
+    parser.add_argument('--to','-t', default='Chicago')
+    parser.add_argument('--steps','-s',default=20)
+    parser.add_argument('--out','-o') # Not yet implemented
+    arguments=parser.parse_args()
+    mygraph=Greengraph(arguments.start,arguments.to) 
+    data = mygraph.green_between(arguments.steps)
+    plt.plot(data)
+    plt.show()
+
+# mygraph=Greengraph('New York','Chicago')
+# data = mygraph.green_between(20)
+# plt.plot(data)
+# plt.show()
