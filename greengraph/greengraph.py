c4a0486506725242543b78f9014b1c04b8522d95
Gavin Dold <gdold42@gmail.com>
2015-12-10 21:17:35 +0000

    Added CITATION and USAGE files, and clarified --help text

--- greengraph/greengraph.py
+++ greengraph/greengraph.py
@@ -6,13 +6,13 @@ from argparse import ArgumentParser
 def runGreengraph():
     parser = ArgumentParser(description = "Generates a graph of the proportion of green pixels in a series of satellite images between two points")
     parser.add_argument('--from', dest='start', default='New York', help='Starting location, defaults to New York') # Python doesn't like 'arguments.from'
-    parser.add_argument('--to', default='Chicago', help='End location, defaults to Chicago')
+    parser.add_argument('--to', dest='end', default='Chicago', help='End location, defaults to Chicago')
     parser.add_argument('--steps', default=20, help='Number of steps between start and end locations')
     parser.add_argument('--out', help='Filename to save plot to. Displays plot on screen instead if not specified')
     parser.add_argument('--format', default=None, help='Optionally force an output format, overriding extension in --out')
     arguments=parser.parse_args()
     
-    mygraph=Greengraph(arguments.start,arguments.to) 
+    mygraph=Greengraph(arguments.start,arguments.end) 
     data = mygraph.green_between(arguments.steps)
     plt.plot(data)
     
