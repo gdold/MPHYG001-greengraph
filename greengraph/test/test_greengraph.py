ce4754f864e0d7ab742bcaac2bdb3088e9511429
Gavin Dold <gdold42@gmail.com>
2015-12-11 15:18:02 +0000

    Work on implementing tests for graph.py

--- greengraph/test/test_greengraph.py
+++ greengraph/test/test_greengraph.py
@@ -1,5 +1,12 @@
 from nose.tools import assert_equal, assert_almost_equal
+import numpy as np
+import geopy
+import requests
+from StringIO import StringIO
+from matplotlib import image as img
+import matplotlib.pyplot as plt
+from argparse import ArgumentParser
+import ..greengraph
 from ..graph import Greengraph
 from ..map import Map
-from mock import patch
-
+from mock import Mock, patch
